a6b9f721dc1bcc299e79a99d9f97d6a0f1d6b1c8
WCDTestAC <161503860+WCDTestAC@users.noreply.github.com>
2024-05-12 22:26:08 +0000

    Initial commit

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd';
+
+const { Text } = Typography;
+//footer
+const Copyright = () => {
+    return (
+        <>
+            <Text>(c) 2024 The Canine Shelter</Text>
+        </>
+    )
+}
+
+export default Copyright
